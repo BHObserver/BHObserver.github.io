36ddbfa4075ac27115fa68ba2e168ce0b52f7ac9
BHObserver <61288243+BHObserver@users.noreply.github.com>
2021-10-07 09:06:48 +0600

    Update index.js

--- moviecompare/index.js
+++ moviecompare/index.js
@@ -11,7 +11,7 @@ const autocompleteConfig = {
         return movie.Title;
     },
     async fetchData(searchTerm) {
-        const response = await axios.get('http://www.omdbapi.com/', {
+        const response = await axios.get('https://www.omdbapi.com/', {
             params: {
                 apikey: 'cae9cc7d', 
                 //i: 'tt0848228'
